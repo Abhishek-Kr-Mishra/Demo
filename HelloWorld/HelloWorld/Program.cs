4dafc9a9e0eef9e45bbd0fcb77342dd1318ad8c2
abh224272 <abh224272@gmail.com>
2020-12-11 13:09:02 +0530

    Add: Implementation of Hello World Program

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
